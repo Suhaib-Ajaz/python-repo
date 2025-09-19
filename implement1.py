54e7bee657e971ca8d30c6778b7763180b161e76
Suhaib Ajaz <suhaibajaz.11@gmail.com>
2025-09-19 11:07:23 +0530

    Version2

--- implement1.py
+++ implement1.py
@@ -1,6 +1,7 @@
 #Expense tracker
 #Category wise expenses
 #Functions to add expenses, sum, category wise breakdown
+import os.path
 
 
 
@@ -12,6 +13,8 @@ def add_expense():
     expenses[exp_category].append(exp_amount)
         
 def sum_expenses():
+    if not expenses:
+        return {}
     choice2=int(input("1 for total sum \n2 for sum by category"))
     if choice2 == 1:
         sum=0
@@ -31,6 +34,28 @@ def sum_expenses():
             print("Category not present")
         return sum
 
+def save_expenses():
+    with open ('data.txt', 'w') as f:
+        f.write(str(expenses))
+        print('saved')
+    f.close()
+
+
+def load_expenses():
+    if os.path.isfile("data.txt"):
+        with open('data.txt', 'r') as f:
+            temp=f.read()
+        f.close()
+        dictionary=eval(temp)
+        return(dictionary) 
+        
+    
+
+def del_expenses():
+    if os.path.exists("data.txt"):
+        os.remove("data.txt")
+    else:
+        print("The file does not exist")
 
 def make_choice(choice):
     
@@ -45,6 +70,13 @@ def make_choice(choice):
         case '3':
             print(expenses)
             return 
+        case '4':
+            save_expenses()
+            return
+        case '5':
+            del_expenses()
+            expenses.clear()
+            return
         case 'q':
             print("Shutting down. Thank You")
             return 
@@ -53,11 +85,17 @@ def make_choice(choice):
             return 
         
 expenses={}
-choice='0'
+choice='0'  
+expenses=load_expenses()
+if not expenses:
+    expenses={}
+
 while choice != 'q':
     print("##Expense Tracker##")
     print("1 to Add Expenses")
     print("2 to Sum Expenses")
     print("3 to Show Expenses")
+    print("4 to Save")
+    print("5 to Delete all data")
     choice=str(input("Press q to quit\nEnter your choice:"))
     make_choice(choice)
